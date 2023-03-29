4fb39634065214d18aa1413307a3ca9902c18a81
Simon Erlic <simon@erlic.ca>
2023-03-28 20:06:17 -0700

    Fix LinkedIn link, spacing on mobile

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Simon Erlic';
-export const SITE_DESCRIPTION = 'Software engineer, student, XKCD fan.';
+export const SITE_TITLE = "Simon Erlic | Student, maker, and Software Developer";
+export const SITE_DESCRIPTION = "Hi, I'm a student, FIRST alumnus, and self-described maker currently living, and attending school, in Victoria, BC.";
