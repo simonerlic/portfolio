de75c3c9394b3142aef56838f241ab3a82cf3635
Simon Erlic <simon@erlic.ca>
2023-04-06 13:33:25 -0700

    Add work for potentially using mesh gradient

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://simonerlic.com',
   integrations: [mdx(), sitemap(), tailwind()],
   output: "server",
   adapter: cloudflare()
