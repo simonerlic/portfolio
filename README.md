e4d19cb06ba5430ab04e83e4c84ed27b003850d4
Simon Erlic <simon@erlic.ca>
2023-10-03 14:44:42 -0700

    clean slate

--- README.md
+++
@@ -1,7 +0,0 @@
-# Personal Portfolio
-
-This repo holds all of the content and source for my pesonal website!
-
-## Technologies
-
-This website uses Astro since I've heard good things about it. So far, so good!
--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "Simon Erlic | Student, maker, and Software Developer";
-export const SITE_DESCRIPTION = "Hi, I'm a student, FIRST alumnus, and self-described maker currently living, and attending school, in Victoria, BC.";
