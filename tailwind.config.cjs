c97c098a1b09ef8493036f66d606a05896efb858
Simon Erlic <simon@erlic.ca>
2023-10-30 22:44:26 -0700

    Add custom fonts (did not take 2 hours I swear)

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
 			fontFamily: {
-				'serif': ['YoungSerif-Regular', 'serif'],
+				'dmserif': ["'DMSerif'", 'serif'],
+				'youngserif': ['YoungSerif', 'serif'],
+				'outfit': ['Outfit', 'sans-serif'],
 			},
 		},
 		colors: {
