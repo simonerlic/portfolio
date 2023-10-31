701ad15e08d607c554a65ddebb4fc570b55e7567
Simon Erlic <simon@erlic.ca>
2023-10-31 16:44:52 -0700

    Add intial about section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,13 +8,18 @@ module.exports = {
 				'dmserif': ["'DMSerif'", 'serif'],
 				'youngserif': ['YoungSerif', 'serif'],
 				'outfit': ['Outfit', 'sans-serif'],
+				'outfit-medium': ['Outfit-Medium', 'sans-serif'],
 			},
 		},
 		colors: {
 			transparent: 'transparent',
 			current: 'currentColor',
 			'slight': "#000000a0",
+			'slighter': "#00000070",
+			'porcelain': "#f8fbf8",
+			'silk': "#dddcd6",
+			'greenish': "#c8d5bb"
 		},
 	},
-	plugins: [],
+	plugins: []
 }
