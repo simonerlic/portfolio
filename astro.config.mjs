ddc22c9198175b56e0b45b305a8aee2b8b22bd5d
Simon Erlic <simon@erlic.ca>
2023-10-31 22:37:47 -0700

    Add animate on scroll for elements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,18 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import react from '@astrojs/react';
 
-// https://astro.build/config
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://simonerlic.com',
-  integrations: [mdx(), sitemap(), tailwind(), react()],
+  integrations: [mdx(), sitemap(), tailwind()],
   output: "server",
   adapter: cloudflare()
 });
